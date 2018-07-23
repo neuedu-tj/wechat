67a552aec0de12d6d60c22e85198b03c01343db8
neuedu-tj <cofcoltd@qq.com>
2018-07-23 16:19:03 +0800

    自动完成-2 , end

--- src/main/webapp/auto-complete/auto.js
+++ src/main/webapp/auto-complete/auto.js
@@ -1,26 +1,29 @@
-
-
-
+var index = 0;
+var total = 0;
 $(window).keyup(function(e){
-
-    //清空 result 层中的子元素
-    $("#result").empty();
-
    if( e.keyCode == 13) { //回车
-
+       $("#name").val($(".selected").text());
+       $("#result").empty();
+       index = 0;
    } else if(e.keyCode == 38) { //up
-
+       if(index == 0) return ;
+       $(".item").addClass("normal").removeClass("selected")
+       $(".item:nth-child("+(--index)+")").addClass("selected").removeClass("normal")
    } else if(e.keyCode ==40) { //down
-
+       if(index == total) return ;
+       $(".item").addClass("normal").removeClass("selected")
+       $(".item:nth-child("+(++index)+")").addClass("selected").removeClass("normal")
    } else {
+       $("#result").empty();
       $.ajax({
           url : 'http://localhost:8080/wechat/auto?name='+$("#name").val(),
           method : 'get',
           dataType : 'json',
           success : function(data) {
 
-              $.each(data , function( i, e ) {
-                 var item = $("<div>"+e.name+"</div>").addClass("item-normal")
+              total = data.length;
+              $.each(data.sort() , function( i, e ) {
+                 var item = $("<div>"+e.name+"</div>").addClass("item").addClass("normal")
                   item.click(function(){
                       $("#name").val(e.name);
                       $("#result").empty();
@@ -30,5 +33,4 @@ $(window).keyup(function(e){
           }
       })
    }
-
 })
