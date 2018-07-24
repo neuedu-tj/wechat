aa6da2df6c9afd2110e1411dae394e3a776235fe
neuedu-tj <cofcoltd@qq.com>
2018-07-24 17:28:14 +0800

    File - upload , 多文件上传

--- src/main/java/servlet/MomentsServlet.java
+++ src/main/java/servlet/MomentsServlet.java
@@ -16,6 +16,7 @@ import javax.servlet.http.Part;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Collection;
 
 @WebServlet("/moments")
 @MultipartConfig
@@ -31,15 +32,24 @@ public class MomentsServlet extends HttpServlet {
         moments.setNickname(nickname);
         moments.setContent(content);
 
-        Part part = request.getPart("img");
+        StringBuilder url = new StringBuilder();
+        Collection<Part> parts = request.getParts();
+
+        for (Part p : parts) {
+            if("img".equalsIgnoreCase(p.getName())) {
+                FileUtils.copyInputStreamToFile( p.getInputStream() , new File("e:/wechat/img/"+p.getSubmittedFileName()));
+                url.append("http://localhost:8080/wechat/img/"+p.getSubmittedFileName()).append(";");
+            }
+        }
+        url.delete(url.length()-1 , url.length());
 
         //文件存入E盘
-        FileUtils.copyInputStreamToFile( part.getInputStream() , new File("e:/wechat/img/"+part.getSubmittedFileName()));
+
 
         //文件的网络访问路径 , 是图片服务器地址
-        String url = "http://localhost:8080/wechat/img/"+part.getSubmittedFileName();
 
-        moments.setImg(url);
+
+        moments.setImg(url.toString());
 
         MomentsDao momentsDao = new MomentsDao();
         int row = momentsDao.saveMoments(moments);
